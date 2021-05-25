95e6ae8cc417a9375ecc392f932d4d256e8f3758
Sergey Fedorov <Hehehehehewarrior@gmail.com>
2021-05-26 00:28:25 +0700

    Add game end

--- spaceship.py
+++ spaceship.py
@@ -1,36 +1,49 @@
 import asyncio
 import curses
 import itertools
-from typing import Tuple
 
 from canvas_constants import CANVAS_FRAME_WIDTH, MIN_CANVAS_COORDINATE
 from curses_tools import draw_frame, read_controls, get_frame_size
 from physics import update_speed
 
 
-def get_spaceship_frames(frame_1_name, frame_2_name) -> Tuple[str, str]:
-    with open(frame_1_name, 'r') as f:
-        frame_1 = f.read()
-
-    with open(frame_2_name, 'r') as f:
-        frame_2 = f.read()
-
-    return frame_1, frame_2
+def get_frame(path: str) -> str:
+    with open(path, 'r') as f:
+        frame = f.read()
+        return frame
 
 
-frame_1, frame_2 = get_spaceship_frames('frames/rocket/rocket_frame_1.txt', 'frames/rocket/rocket_frame_2.txt')
-frame_rows, frame_cols = get_frame_size(frame_1)
+spaceship_frame_1 = get_frame('frames/rocket/rocket_frame_1.txt')
+spaceship_frame_2 = get_frame('frames/rocket/rocket_frame_2.txt')
+spaceship_row_size, spaceship_col_size = get_frame_size(spaceship_frame_1)
+
+gameover_frame = get_frame('frames/game_over.txt')
+
+
+async def show_gameover(canvas):
+    row_size, col_size = get_frame_size(gameover_frame)
+    max_y, max_x = canvas.getmaxyx()
+    frame_start_row = max_y / 2 - row_size
+    frame_start_col = max_x / 2 - col_size / 2
+    while True:
+        draw_frame(
+            canvas=canvas,
+            start_row=frame_start_row,
+            start_column=frame_start_col,
+            text=gameover_frame,
+        )
+        await asyncio.sleep(0)
 
 
 async def animate_spaceship(canvas, start_row, start_column, events, obstacles):
-    draw_frame(canvas, start_row, start_column, frame_1)
+    draw_frame(canvas, start_row, start_column, spaceship_frame_1)
     await draw_spaceship_frames(canvas, start_row, start_column, events, obstacles)
 
 
 async def draw_spaceship_frames(canvas, start_row, start_column, events, obstacles):
     row_speed = column_speed = 0
 
-    for frame in itertools.cycle([frame_1, frame_2]):
+    for frame in itertools.cycle([spaceship_frame_1, spaceship_frame_2]):
         rows_direction, columns_direction, space_pressed = read_controls(canvas)
 
         if space_pressed:
@@ -43,6 +56,12 @@ async def draw_spaceship_frames(canvas, start_row, start_column, events, obstacl
         start_row, start_column = get_spaceship_new_yx(
             start_row, start_column, row_speed, column_speed, canvas,
         )
+
+        for obstacle in obstacles:
+            if obstacle.has_collision(
+                start_row, start_column, spaceship_row_size, spaceship_col_size
+            ):
+                events.append([0, show_gameover(canvas)])
 
         draw_frame(canvas, start_row, start_column, frame)
         await asyncio.sleep(0)
@@ -60,13 +79,13 @@ def get_spaceship_new_yx(
 
     if row < MIN_CANVAS_COORDINATE:
         row = MIN_CANVAS_COORDINATE
-    elif row >= (max_y - frame_rows):
-        row = max_y - frame_rows - CANVAS_FRAME_WIDTH
+    elif row >= (max_y - spaceship_row_size):
+        row = max_y - spaceship_row_size - CANVAS_FRAME_WIDTH
 
     if col < MIN_CANVAS_COORDINATE:
         col = MIN_CANVAS_COORDINATE
-    elif col >= max_x - frame_cols:
-        col = max_x - frame_cols - CANVAS_FRAME_WIDTH
+    elif col >= max_x - spaceship_col_size:
+        col = max_x - spaceship_col_size - CANVAS_FRAME_WIDTH
 
     return row, col
 
@@ -115,9 +134,9 @@ def check_hit(fire_row, fire_column, obstacles: list) -> bool:
 
 def get_fire(canvas):
     max_y, max_x = canvas.getmaxyx()
-    return fire(canvas, max_y/2, max_x/2 + frame_cols/2)
+    return fire(canvas, max_y/2, max_x/2 + spaceship_col_size/2)
 
 
 async def add_fire_event(canvas, row, col, events, obstacles):
-    event = [0, fire(canvas, row, col + frame_cols/2, obstacles=obstacles)]
+    event = [0, fire(canvas, row, col + spaceship_col_size/2, obstacles=obstacles)]
     events.append(event)
